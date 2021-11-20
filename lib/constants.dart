4729f4939e557588b89ae0fe75c226c21dd16326
PUT_YOUR_NAME_HERE <MY_NAME@example.com>
2021-11-20 19:08:38 +0300

    v2

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,9 @@
 const String kadd = "assits_svgs/add.svg";
+const String kbookmark = "assits_svgs/ookmark.svg";
+const String kburger = "assits_svgs/burger.svg";
+const String kcomment = "assits_svgs/comment.svg";
+const String kgrid = "assits_svgs/grid.svg";
+const String khart = "assits_svgs/hart.svg";
+const String khome = "assits_svgs/home.svg";
+const String klogo = "assits_svgs/logo.svg";
+const String knotifications = "assits_svgs/notifications.svg";
